21bebe4c5302f2bc501572e2cc846303a5e9f388
Jeffrey Duda <jeff.duda@gmail.com>
2024-10-16 09:27:59 -0400

    ENH: moving to hl7apy

--- src/hl7utils/__init__.py
+++ src/hl7utils/__init__.py
@@ -4,3 +4,7 @@ from .hl7utils import has_observations
 from .hl7utils import get_reasons_for_study
 from .hl7utils import get_procedure_info
 from .hl7utils import get_text_observations
+from .hl7utils import show_message
+from .hl7utils import message_type
+from .hl7utils import children_names
+
--- src/hl7utils/hl7utils.py
+++ src/hl7utils/hl7utils.py
@@ -1,7 +1,60 @@
 import hl7 
+
+# maybe try this package for build/send/receive of hl7 messages
+import hl7apy
+from hl7apy import parser
+from hl7apy.core import Group, Segment
+from hl7apy.exceptions import UnsupportedVersion
+
 import os
 
-def from_text_file(file_path: str) -> hl7.Message:
+indent = "    "
+indent_seg = "    "
+indent_fld = "        "
+
+def subgroup (group, indent):
+    indent = indent + "    "
+    print (indent , group)
+    for group_segment in group.children:
+        if isinstance(group_segment, Group):
+            subgroup (group_segment)
+        else: 
+            print(indent_seg, indent ,group_segment)
+            for attribute in group_segment.children:
+                print(indent_fld, indent ,attribute, attribute.value)
+
+
+def show_message(file_path: str) -> None:
+    """
+
+    """
+    if not os.path.exists(file_path):
+        raise ValueError('File does not exist: '+file_path)
+
+    with open(file_path, 'r') as file:
+        dat = file.readlines()
+    dat = ''.join(dat).replace('\n', '\r')
+    msg = parser.parse_message(dat)
+
+    print(msg.children[1])
+    for segment in msg.children:
+        if isinstance(segment, Segment):
+            print (indent ,segment)
+            for attribute in segment.children:
+                print(indent_fld, indent, attribute, attribute.value)
+        if isinstance(segment, Group):
+            for group in segment.children:
+                print (indent,group)
+                for group_segment in group.children:
+                    if isinstance (group_segment, Group): 
+                        subgroup (group_segment, indent)
+                    else:
+                        print(indent_seg, indent ,group_segment)
+                        for attribute in group_segment.children:
+                            print(indent_fld, indent, attribute, attribute.value)
+
+
+def from_text_file(file_path: str, find_groups=True) -> hl7apy.core.Message:
     """
 
     """
@@ -13,7 +66,14 @@ def from_text_file(file_path: str) -> hl7.Message:
         msg = file.readlines()
     msg = ''.join(msg).replace('\n', '\r')
 
-    return hl7.parse(msg)
+    return parser.parse_message(msg, find_groups=find_groups)
+
+def message_type(hl7_msg: hl7.Message) -> str:
+    """
+
+    """
+    return hl7_msg.msh.msh_9.value
+
 
 def get_segment_list(hl7_msg: hl7.Message) -> list:
     """
@@ -31,15 +91,31 @@ def has_observations(hl7_msg: hl7.Message) -> bool:
         ret=hl7_msg.segment('ORC')[1][0]=='RE'
     return ret
 
-
-def get_reasons_for_study(hl7_msg: hl7.Message) -> str:
+def children_names(children: list) -> list:
     """
 
     """
-    reasons=None
-    if 'OBR' in get_segment_list(hl7_msg):
-        reasons = ["|".join(r[0]) for r in hl7_msg.segment('OBR')[31]]
+    return [child.name for child in children]
 
+def get_reasons_for_study(hl7_msg: hl7apy.core.Message) -> str:
+    """
+
+    """
+    reasons=[]
+    for i in hl7_msg.children:
+        if i.name == 'ORU_R01_RESPONSE':
+            #print(i.name)
+            for j in i.children:
+                if j.name == 'ORU_R01_ORDER_OBSERVATION':
+                    #print("  " + j.name)
+                    for k in j.children:
+                        if k.name == 'OBR':
+                            #print("    " + k.name)
+                            for l in k.children:
+                                #print("      " + l.name)
+                                if l.name == 'OBR_31':
+                                    #print("        " + l.value)
+                                    reasons.append(l.value)
     return reasons
 
 def get_procedure_info(hl7_msg: hl7.Message) -> str:
